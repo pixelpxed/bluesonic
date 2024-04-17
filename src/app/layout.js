612f768702db9e24b0c565303cc65a056b9ef1ca
PixelEdition <pixel.pxed@gmail.com>
2024-04-18 04:20:39 +0700

    feat: Local Server-Sided Fonts

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,7 @@
-import { Inter } from "next/font/google";
-import "./globals.css";
+import NavBar from "@/components/common/navbar";
 
-const inter = Inter({ subsets: ["latin"] });
+import "./globals.css";
+import "/public/fonts/Line_Seed_Sans_TH/fonts.css"
 
 export const metadata = {
   title: "Create Next App",
@@ -11,7 +11,12 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+        <body>
+          <NavBar />
+          <main className="max-w-5xl m-auto p-4">
+            {children}
+          </main>
+        </body>
     </html>
   );
 }
--- src/app/page.js
+++ src/app/page.js
@@ -1,22 +1,16 @@
-import Image from "next/image";
-import NavBar from "@/components/common/navbar";
-
 export default function Home() {
   return (
-    <body>
-      <NavBar />
-      <main className="max-w-5xl m-auto p-4">
-        <div className="my-16">
-          <div>
-            <h1 className="text-5xl">พี่ว่าแล้วว่าน้องต้องอ่าน แฮร่</h1>
-            <p>สวัสดีครับน้องๆ bluesonic ทุกคน ขอบคุณที่อย่างน้อยๆ ก็ได้เปิดมาอ่านนะ</p>
-          </div>
-          <div>
-            <button type="fill">ดูรูปภาพความทรงจำกันเถอะ</button>
-            <button type="outline">ข้อความจากพี่ๆ</button>
-          </div>
+    <>
+      <div className="my-16">
+        <div>
+          <h1 className="text-5xl">พี่ว่าแล้วว่าน้องต้องอ่าน แฮร่</h1>
+          <p>สวัสดีครับน้องๆ bluesonic ทุกคน ขอบคุณที่อย่างน้อยๆ ก็ได้เปิดมาอ่านนะ</p>
         </div>
-      </main>
-    </body>
+        <div>
+          <button type="fill">ดูรูปภาพความทรงจำกันเถอะ</button>
+          <button type="outline">ข้อความจากพี่ๆ</button>
+        </div>
+      </div>
+    </>
   );
 }
