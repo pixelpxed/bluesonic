3a594c20cd66085017fad5830aa5918a4468e475
PixelEdition <pixel.pxed@gmail.com>
2024-04-18 14:39:05 +0700

    feature: proper button styling

--- src/app/page.js
+++ src/app/page.js
@@ -1,14 +1,14 @@
 export default function Home() {
   return (
     <>
-      <div className="my-16">
-        <div>
+      <div>
+        <div className="my-16">
           <h1 className="text-5xl">พี่ว่าแล้วว่าน้องต้องอ่าน แฮร่</h1>
           <p>สวัสดีครับน้องๆ bluesonic ทุกคน ขอบคุณที่อย่างน้อยๆ ก็ได้เปิดมาอ่านนะ</p>
         </div>
-        <div>
-          <button type="fill">ดูรูปภาพความทรงจำกันเถอะ</button>
-          <button type="outline">ข้อความจากพี่ๆ</button>
+        <div className="m-auto w-full">
+          <button type="filled">ดูรูปภาพความทรงจำกันเถอะ</button>
+          <button type="outlined">ข้อความจากพี่ๆ</button>
         </div>
       </div>
     </>
